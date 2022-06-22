44a23b84eb60daf42b90636f66249f2bae344bf0
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-06-22 17:50:28 +0430

    feat : creat api classes to get requests

---
+++ src/main/java/messenger/api/ChannelApi.java
@@ -0,0 +1,41 @@
+package messenger.api;
+
+import messenger.service.model.request.Channel.*;
+
+public class ChannelApi
+{
+    public void addUsers(AddUsersChannelReq request)
+    {
+
+    }
+
+    public void creatChannel(CreatChannelReq request)
+    {
+
+    }
+
+    public void deleteChannel(DeleteChannelReq request)
+    {
+
+    }
+
+    public void getChatHistory(GetChatHistoryReq request)
+    {
+
+    }
+
+    public void pinMessage(PinMessageReq request)
+    {
+
+    }
+
+    public void removeUsers(RemoveUsersChannelReq request)
+    {
+
+    }
+
+    public void renameChannel(RenameChannelReq request)
+    {
+
+    }
+}
