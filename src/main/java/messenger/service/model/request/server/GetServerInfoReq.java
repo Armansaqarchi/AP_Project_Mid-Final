3cf41c594077e2fda2eb99ab8ad2cfeff025f893
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-06-27 14:30:07 +0430

    refactor : replace get image request with get info request

---
+++ src/main/java/messenger/service/model/request/server/GetServerInfoReq.java
@@ -0,0 +1,8 @@
+package messenger.service.model.request.server;
+
+public class GetServerInfoReq extends ServerReq
+{
+    public GetServerInfoReq(String senderId, String serverId) {
+        super(senderId, ServerRequestType.GET_INFO, serverId);
+    }
+}
