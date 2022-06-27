1c7a49efcca3498de102f6288056b212aeacbaf1
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-06-27 18:04:10 +0430

    feat : handle requests related to channel

--- src/main/java/messenger/api/ChannelApi.java
+++ src/main/java/messenger/api/ChannelApi.java
@@ -1,10 +1,24 @@
 package messenger.api;
 
+import messenger.service.ChannelService;
 import messenger.service.model.exception.InvalidTypeException;
+import messenger.service.model.exception.ServerThreadNotFoundException;
 import messenger.service.model.request.Channel.*;
+import messenger.service.model.response.Response;
 
 public class ChannelApi
 {
+    private final ChannelService service;
+
+    //sender object to send responses to client
+    private final Sender sender;
+
+    public ChannelApi()
+    {
+        service = new ChannelService();
+        sender = Sender.getSender();
+    }
+
 
     public void getRequest(ChannelReq request) throws InvalidTypeException {
         switch(request.subType())
@@ -22,41 +36,53 @@ public class ChannelApi
     }
     private void addUser(AddUserChannelReq request)
     {
-
+        sendResponse(service.addUser(request));
     }
 
     private void creatChannel(CreateChannelReq request)
     {
-
+        sendResponse(service.creatChannel(request));
     }
 
     private void deleteChannel(DeleteChannelReq request)
     {
-
+        sendResponse(service.deleteChannel(request));
     }
 
     private void getChatHistory(GetChatHistoryReq request)
     {
-
+        sendResponse(service.getChatHistory(request));
     }
 
     private void pinMessage(PinMessageReq request)
     {
-
+        sendResponse(service.pinMessage(request));
     }
 
     private void getPinnedMessage(GetPinnedMsgReq request)
     {
-
+        sendResponse(service.getPinnedMessage(request));
     }
 
     private void removeUser(RemoveUserChannelReq request)
     {
-
+        sendResponse(service.removeUser(request));
     }
 
     private void renameChannel(RenameChannelReq request)
     {
+        sendResponse(service.renameChannel(request));
+    }
 
+    private void sendResponse(Response response)
+    {
+        try
+        {
+            sender.sendResponse(response);
+        }
+        catch (ServerThreadNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/messenger/service/model/server/RuleType.java
+++ src/main/java/messenger/service/model/server/RuleType.java
@@ -3,7 +3,10 @@ package messenger.service.model.server;
 public enum RuleType
 {
     CREATE_CHANNEL("creat channel"), DELETE_CHANNEL("delete channel"),
-    REMOVE_MEMBER("Remove member"), RESTRICT_MEMBER("Restrict member"),
+    REMOVE_MEMBER("Remove member"), RENAME_CHANNEL("rename channel"),
+
+    //remove member of channel
+    RESTRICT_MEMBER("Restrict member"),
     RENAME_SERVER("Rename server"), SET_IMAGE("set image") ,
     WATCH_HISTORY("Watch server"), PIN_MESSAGE("Pin message");
     private final String value;
