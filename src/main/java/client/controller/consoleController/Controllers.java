0ef7a055b79012e9555635ad3006bfb8271e1d43
Arman sagharchi <99171905+Armansaqarchi@users.noreply.github.com>
2022-06-29 13:04:05 +0430

    feat : Controllers class added which handles and synchronizes all the controllers.

---
+++ src/main/java/client/controller/consoleController/Controllers.java
@@ -0,0 +1,59 @@
+package client.controller.consoleController;
+
+import client.ClientSocket;
+import model.user.User;
+
+public class Controllers {
+
+    private static Controllers controllers;
+    private final ProfileController profileController;
+    private final ServerController serverController;
+    private final SignInController signInController;
+    private final SignUpController signUpController;
+    private final UserController userController;
+    private final ChannelController channelController;
+
+    private Controllers(ClientSocket clientSocket){
+        this.userController = new UserController(clientSocket);
+        this.profileController = new ProfileController(clientSocket);
+        this.signUpController = new SignUpController(clientSocket);
+        this.serverController = new ServerController(clientSocket);
+        this.channelController = new ChannelController(clientSocket);
+        this.signInController = new SignInController(clientSocket);
+    }
+
+    public static Controllers getControllers(ClientSocket clientSocket) {
+        if(controllers == null){
+            return new Controllers(clientSocket);
+        }
+        return controllers;
+    }
+
+    public static void setControllers(Controllers controllers) {
+        Controllers.controllers = controllers;
+    }
+
+    public ProfileController getProfileController() {
+        return profileController;
+    }
+
+    public ServerController getServerController() {
+        return serverController;
+    }
+
+    public SignInController getSignInController() {
+        return signInController;
+    }
+
+    public SignUpController getSignUpController() {
+        return signUpController;
+    }
+
+    public UserController getUserController() {
+        return userController;
+    }
+
+    public ChannelController getChannelController() {
+        return channelController;
+    }
+}
