7221c887106f5f9c37555ae04204bc93df4429da
Arman sagharchi <99171905+Armansaqarchi@users.noreply.github.com>
2022-07-05 13:43:07 +0430

    feat : login page template implemented

---
+++ src/main/java/client/controller/fxController/LoginController.java
@@ -0,0 +1,26 @@
+package client.controller.fxController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField IDText;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField passwordText;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
+
+}
