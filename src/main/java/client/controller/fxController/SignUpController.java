02d98ce236b12800744a8d0442626c1894f21c61
Arman sagharchi <99171905+Armansaqarchi@users.noreply.github.com>
2022-07-05 16:06:07 +0430

    feat and fix : signUp template is implemented and also a button was added to the login section.

--- src/main/java/client/controller/fxController/SignUpController.java
+++ src/main/java/client/controller/fxController/SignUpController.java
@@ -1,4 +1,42 @@
 package client.controller.fxController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class SignUpController {
+
+    @FXML
+    private TextField IDText;
+
+    @FXML
+    private TextField emailText1;
+
+    @FXML
+    private Button haveAnAccount;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField nameText;
+
+    @FXML
+    private TextField passwordText;
+
+    @FXML
+    private TextField phoneNumberText;
+
+    @FXML
+    void onHaveAnAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
+
 }
