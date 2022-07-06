8932e6e10025c21c6945f8308879ead99ca76712
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-06 15:20:52 +0430

    feat : show my profile scene

--- src/main/java/client/controller/fxController/MyProfileController.java
+++ src/main/java/client/controller/fxController/MyProfileController.java
@@ -1,4 +1,29 @@
 package client.controller.fxController;
 
-public class MyProfileController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class MyProfileController
+{
+    @FXML
+    private Button escape;
+
+    @FXML
+    private Button editId;
+    @FXML
+    private Button editName;
+    @FXML
+    private Button editEmail;
+    @FXML
+    private Button editPhoneNumber;
+
+    @FXML
+    private Button editImage;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
 }
