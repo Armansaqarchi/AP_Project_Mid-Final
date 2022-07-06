9b5b17b61bda74b7e08843309b8d5d0053c63d32
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-06 18:23:30 +0430

    feat : scene of the editProfile section

---
+++ src/main/java/client/controller/fxController/EditProfileController.java
@@ -0,0 +1,28 @@
+package client.controller.fxController;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class EditProfileController
+{
+    @FXML
+    private Label topLabel;
+
+    @FXML
+    private Label editValue;
+
+    @FXML
+    private Label responseMessage;
+
+    @FXML
+    private Button cancel;
+    private Button done;
+
+    @FXML
+    private void eventHandler(ActionEvent event)
+    {
+
+    }
+}
