92913429d80d658c14d32760d2dd0ae103e90345
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-07 11:10:43 +0430

    feat : the status setting scene

---
+++ src/main/java/client/controller/fxController/SetStatusController.java
@@ -0,0 +1,23 @@
+package client.controller.fxController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SetStatusController
+{
+    @FXML
+    private Button online;
+    @FXML
+    private Button idle;
+    @FXML
+    private Button disturb;
+    @FXML
+    private Button invisible;
+
+    @FXML
+    private void eventHandler(ActionEvent event)
+    {
+
+    }
+}
