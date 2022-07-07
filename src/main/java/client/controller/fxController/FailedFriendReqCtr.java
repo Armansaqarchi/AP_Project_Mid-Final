aaca5c7f0a5e75dbee061b337d8abb580962de7b
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-07 12:27:32 +0430

    feat : the scene of failed friend request

---
+++ src/main/java/client/controller/fxController/FailedFriendReqCtr.java
@@ -0,0 +1,18 @@
+package client.controller.fxController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
+public class FailedFriendReqCtr
+{
+    @FXML
+    private Button okay;
+
+    @FXML
+    private void eventHandler(ActionEvent event)
+    {
+
+    }
+}
