859a4d16c1aa049da8bdd709b2107d09686feaf5
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-07 12:28:44 +0430

    fix : change ActionEvents type

--- src/main/java/client/controller/fxController/FailedFriendReqCtr.java
+++ src/main/java/client/controller/fxController/FailedFriendReqCtr.java
@@ -1,9 +1,8 @@
 package client.controller.fxController;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-
-import java.awt.event.ActionEvent;
 
 public class FailedFriendReqCtr
 {
