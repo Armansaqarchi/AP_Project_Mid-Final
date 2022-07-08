eace9051fb3ffe00eabe93cd619dbe16a32213bf
Arman sagharchi <99171905+Armansaqarchi@users.noreply.github.com>
2022-07-08 13:36:36 +0430

    feat : adding getFriends id  section to home controller

--- demo1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.demo1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.demo1 to javafx.fxml;
-    exports com.example.demo1;
-}
