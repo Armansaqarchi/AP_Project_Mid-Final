3645ec6d0113ebc72e1078792b8918669c8ee4b2
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-08 22:03:24 +0430

    feat : the scene of view other users profile

---
+++ src/main/java/client/controller/fxController/CreatServerController.java
@@ -0,0 +1,27 @@
+package client.controller.fxController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CreatServerController
+{
+    @FXML
+    private Button create;
+
+    @FXML
+    private TextField id;
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Label message;
+
+    @FXML
+    private void create(ActionEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/client/controller/fxController/UserProfileController.java
@@ -0,0 +1,62 @@
+package client.controller.fxController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+
+public class UserProfileController
+{
+    @FXML
+    private Button escape;
+    @FXML
+    private Button sendMessage;
+    @FXML
+    private Button viewMore;
+
+    @FXML
+    private Button friend;
+    @FXML
+    private Button block;
+
+    @FXML
+    private Label userId;
+    @FXML
+    private Label name;
+
+    @FXML
+    private Circle image;
+    @FXML
+    private Circle Status;
+
+    @FXML
+    private void escape(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    private void sendMessage(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    private void viewMore(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    private void block(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    private void friend(ActionEvent event)
+    {
+
+    }
+}
