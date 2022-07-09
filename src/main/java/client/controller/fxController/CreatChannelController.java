4b9067808630a35eec770ebe2535b8ea36fab260
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-09 14:39:17 +0430

    feat : add scene of creat channel

---
+++ src/main/java/client/controller/fxController/CreatChannelController.java
@@ -0,0 +1,38 @@
+package client.controller.fxController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class CreatChannelController
+{
+    private String serverId;
+
+    @FXML
+    private Button done;
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private TextField channelName;
+
+    @FXML
+    private Label message;
+
+
+    @FXML
+    private void done(ActionEvent actionEvent)
+    {
+        //incomplete
+    }
+
+    @FXML
+    private void cancel(ActionEvent actionEvent)
+    {
+        //incomplete
+    }
+
+}
