fabc4578194eaf301cbf56603415c0c0ab939a22
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-10 18:48:27 +0430

    feat : implement controller class of failed friend request

--- src/main/java/client/controller/fxController/FailedFriendReqCtr.java
+++ src/main/java/client/controller/fxController/FailedFriendReqCtr.java
@@ -3,15 +3,26 @@ package client.controller.fxController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class FailedFriendReqCtr
 {
     @FXML
+    private GridPane pane;
+
+    @FXML
     private Button okay;
 
     @FXML
-    private void eventHandler(ActionEvent event)
+    private void okay(ActionEvent event)
     {
+        closeScene();
+    }
 
+    private void closeScene()
+    {
+        Stage stage = (Stage)pane.getScene().getWindow();
+        stage.close();
     }
 }
