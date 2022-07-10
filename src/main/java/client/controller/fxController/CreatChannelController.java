db132e14290f080b5ccb22416f640362a9ad5e05
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-10 17:55:14 +0430

    feat : implement controller class of create channel

--- src/main/java/client/controller/fxController/CreatChannelController.java
+++ src/main/java/client/controller/fxController/CreatChannelController.java
@@ -5,11 +5,20 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import model.exception.ResponseNotFoundException;
+import model.request.Channel.CreateChannelReq;
+import model.response.Response;
+import model.server.ChannelType;
 
 
-public class CreatChannelController
+public class CreatChannelController extends Controller
 {
     private String serverId;
+
+    @FXML
+    private GridPane pane;
 
     @FXML
     private Button done;
@@ -24,15 +33,49 @@ public class CreatChannelController
 
 
     @FXML
-    private void done(ActionEvent actionEvent)
+    private void done(ActionEvent event)
     {
-        //incomplete
+        try
+        {
+            clientSocket.send(new CreateChannelReq(clientSocket.getId(), serverId,
+                    channelName.getText(), ChannelType.TEXT));
+
+            Response response = clientSocket.getReceiver().getResponse();
+
+            if(!response.isAccepted())
+            {
+                setMessage(response.getMessage());
+                return;
+            }
+
+            closeScene();
+        }
+        catch(ResponseNotFoundException e)
+        {
+            setMessage("Creating channel failed!");
+        }
     }
 
     @FXML
-    private void cancel(ActionEvent actionEvent)
+    private void cancel(ActionEvent event)
     {
-        //incomplete
+        closeScene();
     }
 
+    private void closeScene()
+    {
+        Stage stage = (Stage)pane.getScene().getWindow();
+        stage.close();
+    }
+
+    private void setMessage(String text)
+    {
+        message.setVisible(true);
+        message.setText(text);
+    }
+
+    public void setServerId(String serverId)
+    {
+        this.serverId = serverId;
+    }
 }
