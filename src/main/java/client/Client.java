4e9a6e092709669f6ffd1753d074e36e3cd69b01
mahdikalhor1 <mahdi.kalhor8181@gmail.com>
2022-07-12 20:45:12 +0430

    feat : switch between client modes

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,13 +1,20 @@
 package client;
 
+import client.controller.fxController.cell.testFx;
 import client.menus.TerminalMenu;
 
+import java.util.Scanner;
 import java.util.concurrent.Executors;
 
 public class Client
 {
     public static void main(String[] args)
     {
+        if(1 == clientMode())
+        {
+            testFx.main(args);
+            return;
+        }
         ClientSocket clientSocket = ClientSocket.getClientSocket();
 
         Executors.newCachedThreadPool().execute(clientSocket);
@@ -15,5 +22,28 @@ public class Client
         TerminalMenu terminalMenu = TerminalMenu.getTerminalMenu(clientSocket);
 
         terminalMenu.connectionMenu();
+    }
+
+    private static int clientMode()
+    {
+        Scanner scanner = new Scanner(System.in);
+        while (true)
+        {
+            System.out.println("choose client mode :");
+            System.out.println("[1] - fx");
+            System.out.println("[2] - terminal");
+
+            switch (scanner.nextLine())
+            {
+                case "1" :
+                {
+                    return 1;
+                }
+                case "2" :
+                {
+                    return 2;
+                }
+            }
+        }
     }
 }
