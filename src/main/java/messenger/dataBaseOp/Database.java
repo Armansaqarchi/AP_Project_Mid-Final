9b3d566126ea8f73eb0ed5cf3e9e8f71048251f4
Arman <arman.saghari81@gmail.com>
2023-03-21 13:37:00 -0400

    fix: fixing database configuration when running the application

--- src/main/java/messenger/dataBaseOp/Database.java
+++ src/main/java/messenger/dataBaseOp/Database.java
@@ -26,7 +26,10 @@ public class Database {
 
             Connection connection = DriverManager.getConnection(
                     "jdbc:postgresql://127.0.0.1:5432/postgres" ,
-                    "postgres" , "postgres");
+                    "mahan" , "test123123");
+
+
+            configure(connection);
 
             //initializing all the operators
             channelOp = new ChannelOp(connection);
@@ -43,6 +46,36 @@ public class Database {
             System.out.println("connection to database failed!");
             throw new RuntimeException(e);
         }
+    }
+
+
+    private void configure(Connection connection){
+        try {
+            Statement statement = connection.createStatement();
+
+            statement.execute("CREATE TABLE IF NOT EXISTS users(user_id varchar(50), name varchar(50)," +
+                    " password varchar(50), email varchar(100), phone_number varchar(20), profile_image BYTEA, user_status varchar," +
+                    "friend_list BYTEA, blocked_users BYTEA, private_chats BYTEA, servers BYTEA, unread_messages BYTEA," +
+                    "friend_requests BYTEA)");
+
+            statement.execute("CREATE TABLE IF NOT EXISTS server" +
+                    "(server_id  varchar(50), name varchar(50), owner_id int, image BYTEA)");
+
+
+            statement.execute("CREATE TABLE IF NOT EXISTS private_chats(id varchar(50) PRIMARY KEY," +
+                    " messages BYTEA)");
+
+            statement.execute("CREATE TABLE IF NOT EXISTS message(message_id varchar(50), sender_id varchar(50), " +
+                    "receiver_id int, type int, date date, reactions BYTEA, content BYTEA)");
+
+            statement.execute("CREATE TABLE IF NOT EXISTS friend_requests(id varchar(50), type varchar(50), sender_id varchar(50), receiver_id varchar(50))");
+            statement.execute("CREATE TABLE IF NOT EXISTS channel(id varchar(50), name varchar(50), channel_type varchar(50)," +
+                    " users BYTEA, messages BYTEA, pinned_messages BYTEA)");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
